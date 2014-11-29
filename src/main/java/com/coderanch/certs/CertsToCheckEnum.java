7ec5b51abe95af6a2f9b1b55e5595a1bf4fcc8c8
Jeanne Boyarsky <nyjeanne@gmail.com>
2014-11-29 16:50:56 -0500

    add more programmer exams

--- src/main/java/com/coderanch/certs/CertsToCheckEnum.java
+++ src/main/java/com/coderanch/certs/CertsToCheckEnum.java
@@ -8,12 +8,21 @@ package com.coderanch.certs;
  */
 public enum CertsToCheckEnum {
 
+	OCJA_JAVA_6(
+			"1Z0-850",
+			"http://education.oracle.com/pls/web_prod-plq-dad/db_pages.getpage?page_id=652&get_params=p_exam_id:1Z0-850"),
+	OCPJP_JAVA_6(
+			"1Z0-851",
+			"http://education.oracle.com/pls/web_prod-plq-dad/db_pages.getpage?page_id=652&get_params=p_exam_id:1Z0-851"),
 	OCAJP_JAVA_7(
 			"1Z0-803",
 			"http://education.oracle.com/pls/web_prod-plq-dad/db_pages.getpage?page_id=5001&get_params=p_exam_id:1Z0-803"), 
 	OCPJP_JAVA_7(
 			"1Z0-804",
 			"http://education.oracle.com/pls/web_prod-plq-dad/db_pages.getpage?page_id=5001&get_params=p_exam_id:1Z0-804"),
+	JAVA_7_UPGRADE(
+			"1Z0-805",
+			"http://education.oracle.com/pls/web_prod-plq-dad/db_pages.getpage?page_id=652&get_params=p_exam_id:1Z0-805"),
 	OCAJP_JAVA_8(
 			"1Z0-808",
 			"http://education.oracle.com/pls/web_prod-plq-dad/db_pages.getpage?page_id=5001&get_params=p_exam_id:1Z0-808");
